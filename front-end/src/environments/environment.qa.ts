e0c880ab596ab2b60983ca1dd7079f7709d504ed
Jorge Hoyos <jhoyosl@globalsys.co>
2021-12-06 21:14:21 -0500

    fix: Order envirmoent.qa.ts index

--- front-end/src/environments/environment.qa.ts
+++ front-end/src/environments/environment.qa.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   url: 'http://api-mdp.globalsys.co/',
-  client_id: '9129b2e3-a874-46f0-950e-6d1602a38cac',
-  client_secret: '4W4UDtYprj8DV9f5PbdozEtKnig53ebZRTHVIqhg',
+  clientId: '9129b2e3-a874-46f0-950e-6d1602a38cac',
+  appKey: '4W4UDtYprj8DV9f5PbdozEtKnig53ebZRTHVIqhg',
 };
