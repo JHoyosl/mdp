b7c7240b08ab0a7f797c14db1c8fbf1039e28ce8
LuisaZapata <78331913+LuisaZapata@users.noreply.github.com>
2022-02-19 07:24:31 -0500

    Se ajustan las palabras con tíldes

---
+++ back-end/app/Constant.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App;
+
+/**
+* Constantes
+* 
+*/
+
+class Constant
+{
+    //banks
+    const BANCOLOMBIA     = 4;
+    const BANCO_OCCIDENTE     = 8;
+
+    //Accounts
+    const BANCOLOMBIA_ACCOUNT_1     = 1;
+    const BANCOLOMBIA_ACCOUNT_2     = 2;
+    const BANCO_OCCIDENTE_ACCOUNT_1 = 3;
+    const BANCO_OCCIDENTE_ACCOUNT_2 = 4;
+
+    //map_local_index
+    const FECHA_MOVIMIENTO     = 5;
+    const VALOR_CREDITO     = 15;
+    const REFERENCIA_1     = 8;
+    const REFERENCIA_2     = 9;
+    const REFERENCIA_3     = 10;
+    const MATCHED     = 1;
+
+}
+
+
