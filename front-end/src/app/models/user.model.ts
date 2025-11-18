2658289ddcbda392616f11de50dae6cde49f298e
Jorge Hoyos <jhoyosl@hotmail.com>
2025-11-18 06:40:50 -0500

    fix docker files

--- front-end/src/app/models/user.model.ts
+++ front-end/src/app/models/user.model.ts
@@ -1,23 +1,20 @@
 export class UserModel {
-    id: number;
-    email: string;
-    password: string;
-    current_company: string;
-    names: string;
-    type: string;
-    last_names: string;
-    token?: string;
+  id: number;
+  email: string;
+  password: string;
+  current_company: string;
+  names: string;
+  type: string;
+  last_names: string;
+  token?: string;
 
-    setValues( Object: any) {
-        this.id = Object.id;
-        this.email = Object.email;
-        this.password = Object.password;
-        this.current_company = Object.current_company;
-        this.names = Object.names;
-        this.type = Object.type;
-        this.last_names = Object.last_names;
-
-    }
-
+  setValues(Object: any) {
+    this.id = Object.id;
+    this.email = Object.email;
+    this.password = Object.password;
+    this.current_company = Object.current_company;
+    this.names = Object.names;
+    this.type = Object.type;
+    this.last_names = Object.last_names;
+  }
 }
-
