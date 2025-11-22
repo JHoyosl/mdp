9d5577ab51c3474493c6005557b90158910033d0
Jorge Hoyos <jhoyosl@hotmail.com>
2025-11-22 16:23:46 -0500

    fix upload account info dates

--- back-end/app/Http/Controllers/Accounting/AccountingController.php
+++ back-end/app/Http/Controllers/Accounting/AccountingController.php
@@ -71,6 +71,7 @@ class AccountingController extends ApiController
                     $request->endDate,
                     $company
                 );
+
             return $this->showOne($data);
         } catch (\Exception $e) {
             return $this->errorResponse($e->getMessage(), $e->getCode());
--- back-end/app/Traits/DatesTrait.php
+++ back-end/app/Traits/DatesTrait.php
@@ -10,11 +10,31 @@ trait DatesTrait
 
   private function transformDate($format, $date)
   {
+    if($format == 'yyyymmdd') {
+      return $this->parseFromYYYYMMDD($date);
+    }
+
     $fixDate = $date;
     if (strlen($date) > 10) {
       $fixDate = substr($date, 0, 10);
     }
     return Carbon::parse($fixDate);
+  }
+
+  /**
+   * Parse date from yyyymmdd format to Y-m-d
+   * @param string|int $date Date in yyyymmdd format (e.g., "20251001" or 20251001)
+   * @return string Date in Y-m-d format (e.g., "2025-10-01")
+   */
+  function parseFromYYYYMMDD($date)
+  {
+    $dateString = (string) $date;
+    
+    if (strlen($dateString) !== 8) {
+      throw new Exception('Date must be in yyyymmdd format (8 digits)');
+    }
+    
+    return Carbon::createFromFormat('Ymd', $dateString);
   }
 
   function getDateFormat($mapId)
